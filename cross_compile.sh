f9d880bb8027adadf1ece3f916997c7d94797f8b
Kai Erik Niermann <kai.niermann@gmail.com>
2023-08-14 10:05:10 +0200

    simplified crosscomp script

--- cross_compile.sh
+++ cross_compile.sh
@@ -4,11 +4,5 @@ docker run --rm --name cross -v "$(pwd):/mnt/" rust-crosscomp
 
 read -p "Do you want to zip the app ? [y / n] " ans
 
-if [ $ans == "y" ]; then 
-    zip -r gtkapp gtkapp
-    rm -r gtkapp
-elif [ $ans == "n" ]; then
-    echo Your packaged gtkapp is in $(pwd)/gtkapp
-else 
-    echo Invalid choice
-fi
+zip -r gtkapp gtkapp
+rm -r gtkapp
